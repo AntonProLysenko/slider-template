a714ea85d9aaec8d42a6d096731143d7e838ced5
boldPro <probold777@gmail.com>
2023-05-21 13:32:43 -0400

    arrows ready

---
+++ script.js
@@ -0,0 +1,20 @@
+const leftArrow = document.querySelector(".left-arrow")
+const rightArrow = document.querySelector(".right-arrow")
+const slider = document.querySelector(".slider");
+
+
+
+
+// function scrollRight() {
+//   if (slider.scrollWidth - slider.clientWidth === slider.scrollLeft) {
+//     slider.scrollTo({
+//       left: 0,
+//       behavior: "smooth",
+//     });
+//   } else {
+//     slider.scrollBy({
+//       left: window.innerWidth,
+//       behavior: "smooth",
+//     });
+//   }
+// }
